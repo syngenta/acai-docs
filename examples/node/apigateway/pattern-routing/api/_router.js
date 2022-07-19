59fb08cfa8bb755bfc61ff463f38e168e6a919ef
Paul Cruse III <paul.cruse@syngenta.com>
2022-07-19 13:15:17 -0500

    almost done

--- examples/node/apigateway/pattern-routing/api/_router.js
+++ examples/node/apigateway/pattern-routing/api/_router.js
@@ -1,11 +1,11 @@
 const {Router} = require('@syngenta-digital/alc').apigateway;
-const ApiTraffic = require('common/api-traffic.logic');
+const ApiTraffic = require('./common/api-traffic.logic');
 
 exports.route = async (event) => {
     const router = new Router({
         routingMode: 'pattern',
         basePath: 'pattern-example',
-        handlerPath: 'api/**/*.controller.js',
+        handlerPattern: 'api/**/*.controller.js',
         schemaPath: 'openapi.yml',
         autoValidate: true, // will automatically validate against openapi.yml
         beforeAll: ApiTraffic.logRequest,
